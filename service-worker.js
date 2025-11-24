a0101ab96baf1985baa0e2b3142ee1bac25bc7c0
Carlos9888 <miyakojunior@gmail.com>
2025-11-24 22:39:15 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,8 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Bisa tambahkan caching kalau mau offline
+});
 
