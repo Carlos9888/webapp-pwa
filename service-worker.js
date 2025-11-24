1e1befcc39348c10d013adc25bdab4e1d51ce9f7
Carlos9888 <miyakojunior@gmail.com>
2025-11-24 23:00:19 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,39 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = 'profit-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
   console.log('Service Worker Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Bisa tambahkan caching kalau mau offline
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// Activate event (optional cleanup old cache)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
 });
 
